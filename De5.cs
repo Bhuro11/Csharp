137fb009daab3ba594b7d2666a422c67d71f15b7
Bhuro11 <119996397+Bhuro11@users.noreply.github.com>
2022-12-12 09:55:04 +0530

    Update De5.cs

--- De5.cs
+++ De5.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 class De5 { 
-    public static void main(String[] args){
+    public static void Main(String[] args){
        /* Console.WriteLine("Enter Number Of Student : ");
         int count = Convert.ToInt32(Console.ReadLine());
         String[] nm = new String[count];
@@ -10,49 +10,53 @@ class De5 {
         int[] m3 = new int[count];
         int[] m4 = new int[count];*/
         
-            Console.Write("Enter Student Name : ");
+            Console.Write("Enter Student Name    : ");
             String nm = Console.ReadLine();
             Console.Write("Enter Student Roll No : ");
             int rn = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Student Csharp Mark : ");
+            Console.Write("Enter Student Csharp Mark (out of 100)   : ");
             int m1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Student Java Mark : ");
+            Console.Write("Enter Student Java Mark (out of 100)     : ");
             int m2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Student Adv. Net Mark : ");
+            Console.Write("Enter Student Adv. Net Mark (out of 100) : ");
             int m3 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Student OS Mark : ");
+            Console.Write("Enter Student OS Mark (out of 100)       : ");
             int m4 = Convert.ToInt32(Console.ReadLine());
             int total = m1 + m2 + m3 + m4;
-        int avg = (m1 + m2 + m3 + m4) / 4;
+            int per = (m1 + m2 + m3 + m4) / 4;
+            Console.WriteLine("");
         Console.WriteLine("----------------------------------------");
-        Console.WriteLine("....Marksheet....");
+        Console.WriteLine(".....Marksheet.....");
         Console.WriteLine("----------------------------------------");
-        Console.WriteLine("Name   Roll No   Csharp   Java   Adv.Net   OS   Total   Avg");
-        Console.WriteLine(nm + "\t " + rn + "\t " + m1 + "\t  " + m2 + "\t  " + m3 + "\t  " + m4 + "\t " + total + "\t " + avg);
+        Console.WriteLine("");
+        Console.WriteLine("Name   Roll No   Csharp   Java   Adv.Net   OS   Total   Percentega");
+        Console.WriteLine(nm + "\t " + rn + "\t " + m1 + "\t  " + m2 + "\t  " + m3 + "\t  " + m4 + "\t " + total + "\t " + per + "%");
+        Console.WriteLine("");
         Console.WriteLine("----------------------------------------");
-        Console.WriteLine("....Grade....");
-        Console.WriteLine("----------------------------------------");
+        Console.WriteLine(".....Grade.....");
+        Console.WriteLine("----------------------------------------"); 
+        Console.WriteLine("");
         if (m1 > 30 && m2 > 30 && m3 > 30 && m4 > 30)
         {
-            if (avg >= 70)
+            if (per >= 70)
             {
-                Console.WriteLine("Distinct");
+                Console.WriteLine("Congratulations....... \n You Are A Distinct");
             }
-            else if (avg >= 60)
+            else if (per >= 60)
             {
-                Console.WriteLine("First");
+                Console.WriteLine("Congratulations....... \n You Are A First");
             }
-            else if (avg >= 50)
+            else if (per >= 50)
             {
-                Console.WriteLine("Secound");
+                Console.WriteLine("Excellent....... \n You Are A Secound");
             }
             else
             {
-                Console.WriteLine("Pass");
-            }
+                Console.WriteLine("Good....... \n You Are A Pass");
+            }                                                                                                                                                                                                         
         }
         else {
-            Console.Write("You are fail");
+            Console.Write("Sorry.....\n Try To  Next Time");
         }
      
         Console.Read();
